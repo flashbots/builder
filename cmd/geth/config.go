flashbots/boost-geth-builder

Add remote relay connection for getting validator data (#11)
* Add remote relay connection for getting validator data
* Add block submission to remote relay
* Adjust readme

cmd/geth/config.go
@@ -166,12 +166,14 @@ func makeFullNode(ctx *cli.Context) (*node.Node, ethapi.Backend) {
 
 	bpConfig := &builder.BuilderConfig{
 		EnableValidatorChecks: ctx.GlobalIsSet(utils.BuilderEnableValidatorChecks.Name),
-		SecretKey:             ctx.GlobalString(utils.BuilderSecretKey.Name),
+		BuilderSecretKey:      ctx.GlobalString(utils.BuilderSecretKey.Name),
+		RelaySecretKey:        ctx.GlobalString(utils.BuilderRelaySecretKey.Name),
 		ListenAddr:            ctx.GlobalString(utils.BuilderListenAddr.Name),
 		GenesisForkVersion:    ctx.GlobalString(utils.BuilderGenesisForkVersion.Name),
 		BellatrixForkVersion:  ctx.GlobalString(utils.BuilderBellatrixForkVersion.Name),
 		GenesisValidatorsRoot: ctx.GlobalString(utils.BuilderGenesisValidatorsRoot.Name),
 		BeaconEndpoint:        ctx.GlobalString(utils.BuilderBeaconEndpoint.Name),
+		RemoteRelayEndpoint:   ctx.GlobalString(utils.BuilderRemoteRelayEndpoint.Name),
 	}
 	backend, eth := utils.RegisterEthService(stack, &cfg.Eth, bpConfig)
 	// Warn users to migrate if they have a legacy freezer format.

flashbots/builder

cmd/geth/config.go
@@ -168,12 +168,14 @@ func makeFullNode(ctx *cli.Context) (*node.Node, ethapi.Backend) {
 
 	bpConfig := &builder.BuilderConfig{
 		EnableValidatorChecks: ctx.IsSet(utils.BuilderEnableValidatorChecks.Name),
-		SecretKey:             ctx.String(utils.BuilderSecretKey.Name),
+		BuilderSecretKey:      ctx.String(utils.BuilderSecretKey.Name),
+		RelaySecretKey:        ctx.String(utils.BuilderRelaySecretKey.Name),
 		ListenAddr:            ctx.String(utils.BuilderListenAddr.Name),
 		GenesisForkVersion:    ctx.String(utils.BuilderGenesisForkVersion.Name),
 		BellatrixForkVersion:  ctx.String(utils.BuilderBellatrixForkVersion.Name),
 		GenesisValidatorsRoot: ctx.String(utils.BuilderGenesisValidatorsRoot.Name),
 		BeaconEndpoint:        ctx.String(utils.BuilderBeaconEndpoint.Name),
+		RemoteRelayEndpoint:   ctx.String(utils.BuilderRemoteRelayEndpoint.Name),
 	}
 
 	backend, eth := utils.RegisterEthService(stack, &cfg.Eth, bpConfig)