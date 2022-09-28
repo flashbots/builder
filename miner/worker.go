e5037ec6b68472c99271a6554b2b10d1a0eafcee
Vitaly Drogan <vitaliy.drogan@gmail.com>
2022-09-28 19:43:14 +0300

    squash more annoying logs (#37)

--- miner/worker.go
+++ miner/worker.go
@@ -1916,7 +1916,6 @@ func totalFees(block *types.Block, receipts []*types.Receipt) *big.Float {
 
 func (w *worker) createProposerPayoutTx(env *environment, recipient *common.Address, profit *big.Int) (*types.Transaction, error) {
 	sender := w.coinbase.String()
-	log.Info(sender)
 	nonce := env.state.GetNonce(w.coinbase)
 	fee := new(big.Int).Mul(big.NewInt(paymentTxGas), env.header.BaseFee)
 	amount := new(big.Int).Sub(profit, fee)
