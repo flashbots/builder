311b9a23ba14be6a97d0b4fb4aa2e959f9ef41fc
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2022-11-22 10:41:33 +0100

    Merge changes from validation (#4)
    * Implement block validation API
    
    * Validate proposer payment assuming its the last transaction in the block (#4)
    
    * Validate that the coinbase and feeRecipient are not blacklisted (#5)
    
    * Validate that the proposer payment has no calldata and its gas usage (#6)
    
    * Validate gas limit is set correctly wrt registered (#8)
    
    * Pass validation-specific config (#9)

--- core/blockchain.go
+++ core/blockchain.go
@@ -38,6 +38,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/state/snapshot"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/core/utils"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/event"
