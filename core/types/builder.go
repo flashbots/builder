e287e3e506ca306de4911e88af47a8bfaa6325d8
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2022-12-06 20:46:05 +0100

    Add miner tests with basic smart contracts

---
+++ core/types/builder.go
@@ -0,0 +1,15 @@
+package types
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+)
+
+type BuilderPayloadAttributes struct {
+	Timestamp             hexutil.Uint64 `json:"timestamp"`
+	Random                common.Hash    `json:"prevRandao"`
+	SuggestedFeeRecipient common.Address `json:"suggestedFeeRecipient,omitempty"`
+	Slot                  uint64         `json:"slot"`
+	HeadHash              common.Hash    `json:"blockHash"`
+	GasLimit              uint64
+}
