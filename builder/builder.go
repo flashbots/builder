e7810b51ed4013f399e924aa1574e77e2e8e5130
madjarevicn <nikola.madjarevic@gmail.com>
2022-12-07 16:43:15 +0100

    Increase submission delay from 0.5s to 1s

--- builder/builder.go
+++ builder/builder.go
@@ -287,7 +287,7 @@ func (b *Builder) runBuildingJob(slotCtx context.Context, proposerPubkey boostTy
 	}
 
 	// resubmits block builder requests every second
-	runRetryLoop(ctx, 500*time.Millisecond, func() {
+	runRetryLoop(ctx, 1*time.Second, func() {
 		log.Debug("retrying BuildBlock", "slot", attrs.Slot, "parent", attrs.HeadHash)
 		err := b.eth.BuildBlock(attrs, blockHook)
 		if err != nil {
