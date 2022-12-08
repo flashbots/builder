33935a50544874222e598add4db6613e995e46ac
madjarevicn <nikola.madjarevic@gmail.com>
2022-12-08 10:21:22 +0100

    Fix

--- builder/builder.go
+++ builder/builder.go
@@ -81,7 +81,7 @@ func NewBuilder(sk *bls.SecretKey, ds flashbotsextra.IDatabaseService, relay IRe
 		builderPublicKey:     pk,
 		builderSigningDomain: builderSigningDomain,
 
-		limiter:       rate.NewLimiter(rate.Every(time.Millisecond), 510),
+		limiter:       rate.NewLimiter(rate.Every(time.Millisecond), 600),
 		slot:          0,
 		slotCtx:       slotCtx,
 		slotCtxCancel: slotCtxCancel,
@@ -286,7 +286,7 @@ func (b *Builder) runBuildingJob(slotCtx context.Context, proposerPubkey boostTy
 	}
 
 	// resubmits block builder requests every second
-	runRetryLoop(ctx, 600*time.Millisecond, func() {
+	runRetryLoop(ctx, 1*time.Second, func() {
 		log.Debug("retrying BuildBlock", "slot", attrs.Slot, "parent", attrs.HeadHash)
 		err := b.eth.BuildBlock(attrs, blockHook)
 		if err != nil {
