876197780cbcc7d7a4fdb74e0f2f67e326508db3
avalonche <avalonche@protonmail.com>
2023-02-08 10:17:45 +1100

    builder submit capella block

--- builder/service.go
+++ builder/service.go
@@ -196,7 +196,7 @@ func Register(stack *node.Node, backend *eth.Ethereum, cfg *Config) error {
 		return errors.New("incorrect builder API secret key provided")
 	}
 
-	builderBackend := NewBuilder(builderSk, ds, relay, builderSigningDomain, ethereumService, cfg.DryRun, validator, backend.APIBackend.ChainConfig())
+	builderBackend := NewBuilder(builderSk, ds, relay, builderSigningDomain, ethereumService, cfg.DryRun, validator)
 	builderService := NewService(cfg.ListenAddr, localRelay, builderBackend)
 
 	stack.RegisterAPIs([]rpc.API{
