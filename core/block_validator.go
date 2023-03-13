2b0eaf287d26815b4b616e954a4f14e00447602e
shana <avalonche@protonmail.com>
2023-03-13 11:39:15 -0700

    Prevent uint256 overflow during conversion (#45)
    * Prevent uint256 overflow during conversion
    
    * add shanghai checks
    
    * remove unused functions
    
    * move validation to ValidatePayload

--- core/block_validator.go
+++ core/block_validator.go
@@ -78,7 +78,7 @@ func (v *BlockValidator) ValidateBody(block *types.Block) error {
 			return fmt.Errorf("withdrawals root hash mismatch (header value %x, calculated %x)", *header.WithdrawalsHash, hash)
 		}
 	}
-
+	
 	if !v.bc.HasBlockAndState(block.ParentHash(), block.NumberU64()-1) {
 		if !v.bc.HasBlock(block.ParentHash(), block.NumberU64()-1) {
 			return consensus.ErrUnknownAncestor
