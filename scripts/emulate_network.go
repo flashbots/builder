2a2511d05ae0761aa33f9de98afa7d6158786a9d
Oleg Jakushkin <oleg.jakushkin@gmail.com>
2023-10-19 12:51:33 +0100

    Runner script (#1)
    * Runner script
    
    Allows image building, kurtosis ethereum-package starting and stopping using the generated image
    
    * feat: update geth builder overview dashboard
    
    Created a Go script that follows the following API and is expected to exist in ./scripts inside builder repo:
    
    Main features:
     - Runs on Windows and Linux using Go run
     - Does progressive command log printing
     - Allows to set ImageTag and provide ImageArgs when building starting the network
    Example commands:
    
     - build ../ repo into docker image go run emulate_network.go build
     - run network and see deployment progress go run emulate_network.go run -n=enclaveName -k="C:/Users/olegj/kurtosis.exe”
     - stop network go run emulate_network.go stop -n=enclaveName -k="C:/Users/olegj/kurtosis.exe”
    
    
    Co-authored-by: Carlos Bermudez Porto <cbermudez.dev@gmail.com>

---
+++ scripts/emulate_network.go
@@ -0,0 +1,236 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"runtime"
+	"sync"
+)
+
+func build(imageTag string, buildDir string, buildDockerfilePath string) {
+	cmd := "docker build" +
+		" -t " + imageTag +
+		" -f " + buildDockerfilePath +
+		" " + buildDir
+	runCommand(cmd)
+}
+
+func update_config(imageTag string, imageArgs string, kurtosisNetworkScriptFolder string, filename ...string) {
+	// Determine the filename. If not provided, default to "network_params.json".
+	var file string
+	if len(filename) > 0 {
+		file = filename[0]
+	} else {
+		file = "network_params.json"
+	}
+
+	// Read the file
+	fileContent, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Println("Error reading the file:", err)
+		return
+	}
+
+	var data map[string]interface{}
+	err = json.Unmarshal(fileContent, &data)
+	if err != nil {
+		fmt.Println("Error unmarshalling the JSON:", err)
+		return
+	}
+
+	// Navigate to the participants array
+	if participants, ok := data["participants"].([]interface{}); ok {
+		for _, participant := range participants {
+			p, ok := participant.(map[string]interface{})
+			if !ok {
+				fmt.Println("Error casting participant to map")
+				continue
+			}
+
+			// Check if the participant is the one with "el_client_type": "geth"
+			if p["el_client_type"] == "REPLACE_WITH_BUILDER" {
+				p["el_client_type"] = "geth"
+				// Modify the participant entry as needed
+				p["el_client_image"] = imageTag
+
+				if extraParamsInterface, ok := p["el_extra_params"].([]interface{}); ok {
+					var extraParams []string
+					for _, paramInterface := range extraParamsInterface {
+						if param, ok := paramInterface.(string); ok {
+							extraParams = append(extraParams, param)
+						} else {
+							fmt.Println("Error: el_extra_params contains a non-string value")
+						}
+					}
+					extraParams = append(extraParams, imageArgs)
+					p["el_extra_params"] = extraParams
+				} else {
+					fmt.Println("Error: el_extra_params is not an array of strings")
+				}
+
+				fmt.Println("Detected and updated BUILDER config: ", p)
+				break // Exit the loop once the participant is found and updated
+			}
+		}
+	} else {
+		fmt.Println("Participants field not found or not an array")
+		return
+	}
+
+	// Marshal the map back to JSON
+	modifiedContent, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		fmt.Println("Error marshalling the map back to JSON:", err)
+		return
+	}
+
+	// Print out the resulting string
+	fmt.Println(string(modifiedContent))
+
+	// Save the modified content back to the file
+	err = ioutil.WriteFile(kurtosisNetworkScriptFolder+"/network_params_tmp.json", modifiedContent, os.ModePerm)
+	if err != nil {
+		fmt.Println("Error writing the modified content to the file:", err)
+	}
+}
+
+func run(imageTag, imageArgs, enclaveName string, maxSteps int, kurtosisPath, kurtosisNetworkScriptFolder string, kurtosisNetConfigPath string) {
+	/*	params := map[string]interface{}{
+		"image_tag":               imageTag,
+		"image_args":              imageArgs,
+		"enclave_name":            enclaveName,
+		"max_steps":               maxSteps,
+		"kurtosis_path":           kurtosisPath,
+		"kurtosis_network_config": kurtosisNetworkScriptFolder,
+	}*/
+	update_config(imageTag, imageArgs, kurtosisNetworkScriptFolder, kurtosisNetConfigPath)
+
+	cmd := fmt.Sprintf("%s run --enclave %s %s", kurtosisPath, enclaveName, kurtosisNetworkScriptFolder)
+	runCommand(cmd)
+}
+
+func stop(kurtosisPath, enclaveName string) {
+	if enclaveName == "" {
+		fmt.Println("Error: enclave name must be specified.")
+		cmd := fmt.Sprintf("%s enclave ls", kurtosisPath)
+		runCommand(cmd)
+		return
+	}
+	cmd := fmt.Sprintf("%s enclave rm -f %s", kurtosisPath, enclaveName)
+	runCommand(cmd)
+}
+
+func runCommand(cmd string) {
+	var command *exec.Cmd
+	if runtime.GOOS == "windows" {
+		fmt.Println("Running on Windows:", cmd)
+		command = exec.Command("cmd", "/C", cmd)
+	} else {
+		fmt.Println("Running:", cmd)
+		command = exec.Command("sh", "-c", cmd)
+	}
+
+	stdoutPipe, err := command.StdoutPipe()
+	if err != nil {
+		fmt.Printf("Error obtaining stdout: %v\n", err)
+		return
+	}
+
+	stderrPipe, err := command.StderrPipe()
+	if err != nil {
+		fmt.Printf("Error obtaining stderr: %v\n", err)
+		return
+	}
+
+	err = command.Start()
+	if err != nil {
+		fmt.Printf("Command start failed: %v\n", err)
+		return
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		scanner := bufio.NewScanner(stdoutPipe)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		scanner := bufio.NewScanner(stderrPipe)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
+
+	wg.Wait()
+
+	err = command.Wait()
+	if err != nil {
+		fmt.Printf("Command execution failed: %v\n", err)
+	}
+}
+
+func help() {
+	fmt.Println(`Emulate Network script
+Available commands:
+- build
+  - -t : Image tag (optional, default: "flashbots/builder:dev")
+  - -d : Image Build directory (optional, default: "..")
+  - -f : Build dockerfile path (optional, default: "../Dockerfile")
+- run
+  - -t : Image tag (optional, default: "flashbots/builder:dev")
+  - -n : Enclave name (optional, default: "explorer")
+  - -a : Additional builder arguments (optional)
+  - -s : Max steps (optional, default: -1)
+  - -k : Kurtosis path (optional, default: "kurtosis")
+  - -c : Kurtosis network config (optional, default: "./kurtosis")
+- stop
+  - -k : Kurtosis path (optional, default: "kurtosis")
+  - -n : Enclave name (required)
+`)
+}
+
+func main() {
+	flagSet := flag.NewFlagSet("", flag.ExitOnError)
+	imageTag := flagSet.String("t", "flashbots/builder:dev", "Image tag for build or run.")
+	enclaveName := flagSet.String("n", "explorer", "Enclave name for run or stop.")
+	kurtosisPath := flagSet.String("k", "kurtosis", "Kurtosis path for run or stop.")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a command. Available commands are: build, run, stop.")
+		return
+	}
+
+	switch os.Args[1] {
+	case "build":
+		buildDir := flagSet.String("d", "..", "Build directory.")
+		buildDockerfilePath := flagSet.String("f", "../Dockerfile", "Build dockerfile path.")
+		flagSet.Parse(os.Args[2:])
+		build(*imageTag, *buildDir, *buildDockerfilePath)
+	case "run":
+		imageArgs := flagSet.String("a", "", "Image arguments for run.")
+		maxSteps := flagSet.Int("s", -1, "Max steps for run.")
+		kurtosisNetworkConfigScriptFolder := flagSet.String("f", "./kurtosis", "Kurtosis network config for run.")
+		kurtosisNetConfigPath := flagSet.String("c", "./kurtosis/network_params.json", "Kurtosis network params "+
+			"configuration path. Note that run command modifies it with provided imageTag and imageArgs.")
+		flagSet.Parse(os.Args[2:])
+		run(*imageTag, *imageArgs, *enclaveName, *maxSteps, *kurtosisPath, *kurtosisNetworkConfigScriptFolder, *kurtosisNetConfigPath)
+	case "stop":
+		flagSet.Parse(os.Args[2:])
+		stop(*kurtosisPath, *enclaveName)
+	case "help":
+		help()
+	default:
+		fmt.Println("Invalid command. Available commands are: build, run, stop.")
+	}
+}
---
+++ scripts/readme.md
@@ -0,0 +1,85 @@
+# Emulate Network script
+
+This README accompanies the `emulate_network.go` Go script, which uses Docker, Kurtosis, and the Kurtosis Ethereum-Package enclave to test builder performance.
+
+## Introduction
+
+This script streamlines and automates the process of (re-)building the current builder Docker image and interfacing with the Kurtosis platform. It offers a set of commands to assist developers in routine tasks, such as (re-)building Docker images and managing Kurtosis enclaves.
+
+
+## Running in dev environment
+
+### Prerequisites
+
+Before using the script, ensure you have the following installed:
+
+1. **Go**: Ensure you have Go installed. Download and install it from [here](https://golang.org/dl/).
+
+2. **Docker**: The build process needs Docker. Make sure Docker is installed and running. Check out the [Docker website](https://www.docker.com/get-started) for installation instructions.
+
+3. **Kurtosis**: The script interfaces with the Kurtosis platform. Ensure `kurtosis` is installed and available in your PATH. Refer to [Kurtosis's official documentation](https://docs.kurtosistech.com/installation.html) for installation details.
+
+4. **ethereum-package**: This script utilizes a modified ethereum-package network configuration, which will be downloaded from [repo](github.com/kurtosis-tech/ethereum-package/) main brunch automatically.
+
+### How to Run
+
+To execute the script with Go, navigate to the directory containing the script and run:
+
+```
+go run emulate_network.go <COMMAND_NAME> [OPTIONS]
+```
+
+Replace `<COMMAND_NAME>` with one of the available commands and provide the necessary options.
+
+### Commands and Options
+To run script `cd` into this (`./scripts`) folder.
+
+1. **build**:
+   - Purpose: Builds a Docker image of the builder.
+   - Options:
+      - `-t`: (Optional) Image tag for the Docker build. Defaults to `flashbots/builder:dev`.
+   - Example:
+     ```
+     go run emulate_network.go build -t=test-builder
+     ```
+
+2. **run**:
+   - Purpose: Prepares configurations and starts a Kurtosis enclave.
+   - Options:
+      - `-t`: (Optional) Image tag. Defaults to `flashbots/builder:dev`.
+      - `-n`: (Optional) Enclave name. Defaults to `explorer`.
+      - `-a`: (Optional) Additional builder arguments.
+      - `-s`: (Optional) Max steps (integer). Default `-1` for "unlimited".
+      - `-k`: (Optional) Path to `kurtosis` executable. Defaults to `kurtosis`.
+   - Example:
+     ```
+     go run emulate_network.go run -t=imageTag -a=imageArgs -n=enclaveName -k=/path/to/kurtosis
+     ```
+
+3. **stop**:
+   - Purpose: Stops an active Kurtosis enclave.
+   - Options:
+      - `-k`: (Optional) Path to `kurtosis` executable. Defaults to `kurtosis`.
+      - `-n`: (Required) Enclave name.
+   - Example:
+     ```
+     go run emulate_network.go stop -k=/path/to/kurtosis -n=enclaveName
+     ```
+
+4. **help**:
+   - Purpose: Display a summary of available commands and their options.
+   - Example:
+     ```
+     go run emulate_network.go help
+     ```
+
+## Known issues
+### Kurtosis errors on network start
+In case of system resource related errors or on windows docker restore after sleep Kurtosis may have trouble starting a new enclave. 
+1. Make sure you have no valuable containers up and running in enclaves or docker
+2. To clean up Kurtosis call `kurtosis clean -a`
+3. To clean up docker run `docker rm -vf $(docker ps -aq)`
+
+
+## Running in Docker in Docker
+See this note for refrence here
