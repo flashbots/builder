8d4b3f33786d3597340eaadf1d768f7e50ea9906
Tymur Khrushchov <tkhrushchov@gmail.com>
2023-11-06 16:54:44 +0100

    add retry logic

--- flashbotsextra/rpc_block_service.go
+++ flashbotsextra/rpc_block_service.go
@@ -6,7 +6,6 @@ import (
 
 	apiv1 "github.com/attestantio/go-builder-client/api/v1"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/log"
 	"github.com/flashbots/go-utils/jsonrpc"
 )
 
@@ -18,10 +17,7 @@ func NewRpcBlockClient(URL string) *RpcBlockClient {
 	return &RpcBlockClient{URL: URL}
 }
 
-func (r *RpcBlockClient) ConsumeBuiltBlock(block *types.Block, blockValue *big.Int, ordersClosedAt time.Time, sealedAt time.Time,
-	commitedBundles []types.SimulatedBundle, allBundles []types.SimulatedBundle,
-	usedSbundles []types.UsedSBundle, bidTrace *apiv1.BidTrace,
-) {
+func (r *RpcBlockClient) ConsumeBuiltBlock(block *types.Block, blockValue *big.Int, ordersClosedAt time.Time, sealedAt time.Time, commitedBundles []types.SimulatedBundle, allBundles []types.SimulatedBundle, usedSbundles []types.UsedSBundle, bidTrace *apiv1.BidTrace) error {
 	reqrpc := jsonrpc.JSONRPCRequest{
 		ID:      nil,
 		Method:  "block_consumeBuiltBlock",
@@ -31,8 +27,10 @@ func (r *RpcBlockClient) ConsumeBuiltBlock(block *types.Block, blockValue *big.I
 
 	resp, err := jsonrpc.SendJSONRPCRequest(reqrpc, r.URL)
 	if err != nil {
-		log.Error("could not send rpc request", "err", err)
-	} else {
-		log.Info("successfully relayed data to block processor via json rpc", "resp", string(resp.Result))
+		return err
 	}
+	if resp.Error != nil {
+		return resp.Error
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -77,6 +77,7 @@ require (
 
 require (
 	github.com/bits-and-blooms/bitset v1.5.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.23.0 // indirect
